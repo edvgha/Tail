c7f4327d7474076dab2211f0d1318ef16094cfa7
edvgha <edvard_gh@yahoo.com>
2024-03-27 06:08:09 +0400

    client animation

--- client/simulator.py
+++ client/simulator.py
@@ -1,5 +1,5 @@
 import logging
-
+from collections import deque
 import context
 import client
 import auction
@@ -12,9 +12,16 @@ class Simulator:
         self.signal = False
         self.log = log
         self.auction = auction.Auction(self.context.min_price, self.context.max_price, log)
+        self.d = deque()
 
     def stop(self):
         self.signal = True
+
+    def save(self, bid_price: float, optimized_price: float):
+        true_best_price = self.auction.optimal_price_until(bid_price)
+        if len(self.d) > 2:
+            self.d.popleft()
+        self.d.append((true_best_price, optimized_price, bid_price))
 
     def run(self):
         while not self.signal:
@@ -26,6 +33,7 @@ class Simulator:
             if bid_response.optimized_price > bid_response.price_to_bid:
                 raise Exception("simulator.run(): optimized price > bid price")
             if bid_response.status != "explored":
+                self.save(bid_response.price_to_bid, bid_response.optimized_price)
                 continue
             impression = self.auction.step(bid_response.optimized_price)
 
